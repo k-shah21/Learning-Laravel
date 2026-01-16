2fe531936e4561efda91cbaccc2c377e1bc23811
k-shah21 <kshah@fossphorus.com>
2026-01-16 15:12:55 +0500

    Refactor job routes to use resource controller for cleaner code

--- routes/web.php
+++ routes/web.php
@@ -9,14 +9,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
-
-Route::get('/jobs', [JobController::class, 'index'])->name('jobs.index');
-Route::get('/jobs/create', [JobController::class, 'create'])->name('jobs.create');
-Route::post('/jobs', [JobController::class, 'store'])->name('jobs.store');
-Route::get('/jobs/{job}', [JobController::class, 'show'])->name('jobs.show');
-Route::get('/jobs/{job}/edit', [JobController::class, 'edit'])->name('jobs.edit');
-Route::patch('/jobs/{job}', [JobController::class, 'update'])->name('jobs.update');
-Route::delete('/jobs/{job}', [JobController::class, 'destroy'])->name('jobs.destroy');
+Route::resource('jobs', JobController::class);
 
 
 
